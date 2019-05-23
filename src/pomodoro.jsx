3b590e54f07f62fb3fad03769f609c22dbef073e
Rebecca Viner <rtviner@gmail.com>
2019-05-22 21:19:05 -0600

    add initial files for pomodoro clock

---
+++ src/pomodoro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render () {
+        return (
+            <h2>HELLO</h2>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
